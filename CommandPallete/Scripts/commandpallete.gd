aafa0dabccf036b48daaacb5e912fc6b5f8aac64
GeorgeAct <georgeagbde@gmail.com>
2025-09-26 21:50:41 +0100

    Figured out method for halting key movements when focused on the command Line

--- CommandPallete/Scripts/commandpallete.gd
+++ CommandPallete/Scripts/commandpallete.gd
@@ -1,14 +1,74 @@
 extends Control
 
-@onready var active = false
+@onready var active
+@onready var buttonActive
+@onready var config = ConfigFile.new()
+@onready var err = config.load("res://project.godot")
+var input_values = {}
 
-func toggleCommand() -> void:
+var textures = {
+	true : "./CommandPallete/Textures/Buttons/Dropdown2.png",
+	false: "./CommandPallete/Textures/Buttons/Dropdown1.png"
+}
+
+var toggles = {
+	true : enableCommand,
+	false: disableCommand
+}
+
+func switch_texture() -> void:
+	var texture = load(textures[buttonActive])
+	$Panel/TextureButton.set_texture_normal(texture)
+
+func enableCommand() -> void:
 	visible = active
+	$Panel/LineEdit.grab_focus()
+	$Panel/LineEdit.clear()
+	$Panel/LineEdit.editable = true
+	$Panel/LineEdit.grab_focus()
+
+func disableCommand() -> void:
+	$Panel/LineEdit.release_focus()
+	visible = active
+	$Panel/LineEdit.editable = false
+	$Panel/LineEdit.clear()
 
 func _ready() -> void:
-	toggleCommand()
+	toggles[active].call()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("togglecommand"):
 		active = !active
-		toggleCommand()
+		toggles[active].call()
+
+func _on_texture_button_pressed() -> void:
+	buttonActive = !buttonActive
+	print(buttonActive)
+	switch_texture()
+
+func _on_texture_button_ready() -> void:
+	active = false
+	buttonActive = false
+	switch_texture()
+
+#if err == OK:
+		#print("\n1")
+		#for input in config.get_section_keys("input"):
+			#if input != "togglecommand":
+				#print(input)
+				#input_values[input] = config.get_value("input", input)
+				#config.set_value("input", input, null)
+				#print(input_values[input])
+		#print("\n2")
+		#for input in config.get_section_keys("input"):
+			#print(input)
+			#var value = config.get_value("input", input)
+			#print(value)
+		#print("\n3")
+		#for input in input_values:
+			#config.set_value("input", input, input_values[input])
+		#print("\n4")
+		#for input in config.get_section_keys("input"):
+			#print(input)
+			#var value = config.get_value("input", input)
+			#print(value)
--- CommandPallete/Scripts/texture_button.gd
+++ CommandPallete/Scripts/texture_button.gd
@@ -1,11 +1,16 @@
 extends TextureButton
 
 @onready var active = false
+@onready var config = ConfigFile.new()
+@onready var err = config.load("res://project.godot")
 
 var textures = {
 	true : "./CommandPallete/Textures/Buttons/Dropdown2.png",
 	false: "./CommandPallete/Textures/Buttons/Dropdown1.png"
 }
+
+
+var input_values = {}
 
 func switch_texture() -> void:
 	var texture = load(textures[active])
@@ -18,5 +23,26 @@ func _on_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_ready() -> void:
+	if err == OK:
+		print("\n1")
+		for input in config.get_section_keys("input"):
+			if input != "togglecommand":
+				print(input)
+				input_values[input] = config.get_value("input", input)
+				config.set_value("input", input, null)
+				print(input_values[input])
+		print("\n2")
+		for input in config.get_section_keys("input"):
+			print(input)
+			var value = config.get_value("input", input)
+			print(value)
+		print("\n3")
+		for input in input_values:
+			config.set_value("input", input, input_values[input])
+		print("\n4")
+		for input in config.get_section_keys("input"):
+			print(input)
+			var value = config.get_value("input", input)
+			print(value)
 	switch_texture()
 	pass # Replace with function body.
